83e631967b46dec87395e02e2993f37c6e1356ba
sofaspawn <h.m1nus.work@gmail.com>
2024-09-14 18:54:10 +0530

    first steps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
+fn read_files()->Vec<String>{
+    let paths = fs::read_dir("./text-files").unwrap();
+    let mut texts: Vec<String> = Vec::new();
+
+    for path in paths{
+        let text = fs::read_to_string(path.unwrap().path()).unwrap();
+        texts.push(text);
+    }
+
+    texts
+}
+
 fn main() {
-    println!("Hello, world!");
+    let texts = read_files();
+    for text in texts{
+        println!("{}",text);
+    }
 }
